9e26823a641c59082a72f6b79e02cb87eae49c2f
mourgh21 <mourgh21@gmail.com>
2023-09-07 04:01:35 -0700

    tasks complated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point 
- *
- * Return: always 0 (Success)
+ * Return: 0 (Success)
  */
 int main (void){
     puts("\"Programming is like building a multilingual puzzle");
